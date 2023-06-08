9dc301db8de601ead5acad1ce108d1c713d3ee49
Ubuntu <ubuntu@ip-172-31-68-21.eu-west-1.compute.internal>
2023-06-08 16:43:09 +0000

    Deploying smart contracts script config

--- test-network/scripts/envVar.sh
+++ test-network/scripts/envVar.sh
@@ -9,16 +9,20 @@
 source scriptUtils.sh
 
 export CORE_PEER_TLS_ENABLED=true
-export ORDERER_CA=${PWD}/organizations/ordererOrganizations/example.com/orderers/orderer.example.com/msp/tlscacerts/tlsca.example.com-cert.pem
-export PEER0_ORG1_CA=${PWD}/organizations/peerOrganizations/org1.example.com/peers/peer0.org1.example.com/tls/ca.crt
-export PEER0_ORG2_CA=${PWD}/organizations/peerOrganizations/org2.example.com/peers/peer0.org2.example.com/tls/ca.crt
-export PEER0_ORG3_CA=${PWD}/organizations/peerOrganizations/org3.example.com/peers/peer0.org3.example.com/tls/ca.crt
+export ORDERER_CA=${PWD}/organizations/ordererOrganizations/bcfm.com/orderers/orderer.bcfm.com/msp/tlscacerts/tlsca.bcfm.com-cert.pem
+export PEER0_UA_CA=${PWD}/organizations/peerOrganizations/ua.bcfm.com/peers/peer0.ua.bcfm.com/tls/ca.crt
+export PEER0_AGENCY_CA=${PWD}/organizations/peerOrganizations/agency.bcfm.com/peers/peer0.agency.bcfm.com/tls/ca.crt
+export PEER0_TRANSPORT_CA=${PWD}/organizations/peerOrganizations/transport.bcfm.com/peers/peer0.transport.bcfm.com/tls/ca.crt
+export PEER0_PRODUCER_CA=${PWD}/organizations/peerOrganizations/producer.bcfm.com/peers/peer0.producer.bcfm.com/tls/ca.crt
+export PEER0_PROVIDER_CA=${PWD}/organizations/peerOrganizations/provider.bcfm.com/peers/peer0.provider.bcfm.com/tls/ca.crt
+export PEER0_FARMACY_CA=${PWD}/organizations/peerOrganizations/farmacy.bcfm.com/peers/peer0.farmacy.bcfm.com/tls/ca.crt
+
 
 # Set OrdererOrg.Admin globals
 setOrdererGlobals() {
   export CORE_PEER_LOCALMSPID="OrdererMSP"
-  export CORE_PEER_TLS_ROOTCERT_FILE=${PWD}/organizations/ordererOrganizations/example.com/orderers/orderer.example.com/msp/tlscacerts/tlsca.example.com-cert.pem
-  export CORE_PEER_MSPCONFIGPATH=${PWD}/organizations/ordererOrganizations/example.com/users/Admin@example.com/msp
+  export CORE_PEER_TLS_ROOTCERT_FILE=${PWD}/organizations/ordererOrganizations/bcfm.com/orderers/orderer.bcfm.com/msp/tlscacerts/tlsca.bcfm.com-cert.pem
+  export CORE_PEER_MSPCONFIGPATH=${PWD}/organizations/ordererOrganizations/bcfm.com/users/Admin@bcfm.com/msp
 }
 
 # Set environment variables for the peer org
@@ -31,26 +35,47 @@ setGlobals() {
   fi
   infoln "Using organization ${USING_ORG}"
   if [ $USING_ORG -eq 1 ]; then
-    export CORE_PEER_LOCALMSPID="Org1MSP"
-    export CORE_PEER_TLS_ROOTCERT_FILE=$PEER0_ORG1_CA
-    export CORE_PEER_MSPCONFIGPATH=${PWD}/organizations/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp
-    export CORE_PEER_ADDRESS=peer0.org1.example.com:7051
-    export CORE_PEER_TLS_CERT_FILE=${PWD}/organizations/peerOrganizations/org1.example.com/peers/peer0.org1.example.com/tls/server.crt
-    export CORE_PEER_TLS_KEY_FILE=${PWD}/organizations/peerOrganizations/org1.example.com/peers/peer0.org1.example.com/tls/server.key
+    export CORE_PEER_LOCALMSPID="UaMSP"
+    export CORE_PEER_TLS_ROOTCERT_FILE=$PEER0_UA_CA
+    export CORE_PEER_MSPCONFIGPATH=${PWD}/organizations/peerOrganizations/ua.bcfm.com/users/Admin@ua.bcfm.com/msp
+    export CORE_PEER_ADDRESS=peer0.ua.bcfm.com:7051
+    export CORE_PEER_TLS_CERT_FILE=${PWD}/organizations/peerOrganizations/ua.bcfm.com/peers/peer0.ua.bcfm.com/tls/server.crt
+    export CORE_PEER_TLS_KEY_FILE=${PWD}/organizations/peerOrganizations/ua.bcfm.com/peers/peer0.ua.bcfm.com/tls/server.key
   elif [ $USING_ORG -eq 2 ]; then
-    export CORE_PEER_LOCALMSPID="Org2MSP"
-    export CORE_PEER_TLS_ROOTCERT_FILE=$PEER0_ORG2_CA
-    export CORE_PEER_MSPCONFIGPATH=${PWD}/organizations/peerOrganizations/org2.example.com/users/Admin@org2.example.com/msp
-    export CORE_PEER_ADDRESS=peer0.org2.example.com:9051
-    export CORE_PEER_TLS_CERT_FILE=${PWD}/organizations/peerOrganizations/org2.example.com/peers/peer0.org2.example.com/tls/server.crt
-    export CORE_PEER_TLS_KEY_FILE=${PWD}/organizations/peerOrganizations/org2.example.com/peers/peer0.org2.example.com/tls/server.key
+    export CORE_PEER_LOCALMSPID="AgencyMSP"
+    export CORE_PEER_TLS_ROOTCERT_FILE=$PEER0_AGENCY_CA
+    export CORE_PEER_MSPCONFIGPATH=${PWD}/organizations/peerOrganizations/agency.bcfm.com/users/Admin@agency.bcfm.com/msp
+    export CORE_PEER_ADDRESS=peer0.agency.bcfm.com:9051
+    export CORE_PEER_TLS_CERT_FILE=${PWD}/organizations/peerOrganizations/agency.bcfm.com/peers/peer0.agency.bcfm.com/tls/server.crt
+    export CORE_PEER_TLS_KEY_FILE=${PWD}/organizations/peerOrganizations/agency.bcfm.com/peers/peer0.agency.bcfm.com/tls/server.key
   elif [ $USING_ORG -eq 3 ]; then
-    export CORE_PEER_LOCALMSPID="Org3MSP"
-    export CORE_PEER_TLS_ROOTCERT_FILE=$PEER0_ORG3_CA
-    export CORE_PEER_MSPCONFIGPATH=${PWD}/organizations/peerOrganizations/org3.example.com/users/Admin@org3.example.com/msp
-    export CORE_PEER_ADDRESS=peer0.org3.example.com:11051
-    export CORE_PEER_TLS_CERT_FILE=${PWD}/organizations/peerOrganizations/org3.example.com/peers/peer0.org3.example.com/tls/server.crt
-    export CORE_PEER_TLS_KEY_FILE=${PWD}/organizations/peerOrganizations/org3.example.com/peers/peer0.org3.example.com/tls/server.key
+    export CORE_PEER_LOCALMSPID="TransportMSP"
+    export CORE_PEER_TLS_ROOTCERT_FILE=$PEER0_TRANSPORT_CA
+    export CORE_PEER_MSPCONFIGPATH=${PWD}/organizations/peerOrganizations/transport.bcfm.com/users/Admin@transport.bcfm.com/msp
+    export CORE_PEER_ADDRESS=peer0.transport.bcfm.com:11051
+    export CORE_PEER_TLS_CERT_FILE=${PWD}/organizations/peerOrganizations/transport.bcfm.com/peers/peer0.transport.bcfm.com/tls/server.crt
+    export CORE_PEER_TLS_KEY_FILE=${PWD}/organizations/peerOrganizations/transport.bcfm.com/peers/peer0.transport.bcfm.com/tls/server.key
+  elif [ $USING_ORG -eq 4 ]; then
+    export CORE_PEER_LOCALMSPID="ProducerMSP"
+    export CORE_PEER_TLS_ROOTCERT_FILE=$PEER0_PRODUCER_CA
+    export CORE_PEER_MSPCONFIGPATH=${PWD}/organizations/peerOrganizations/producer.bcfm.com/users/Admin@producer.bcfm.com/msp
+    export CORE_PEER_ADDRESS=peer0.producer.bcfm.com:13051
+    export CORE_PEER_TLS_CERT_FILE=${PWD}/organizations/peerOrganizations/producer.bcfm.com/peers/peer0.producer.bcfm.com/tls/server.crt
+    export CORE_PEER_TLS_KEY_FILE=${PWD}/organizations/peerOrganizations/producer.bcfm.com/peers/peer0.producer.bcfm.com/tls/server.key
+  elif [ $USING_ORG -eq 5 ]; then
+    export CORE_PEER_LOCALMSPID="ProviderMSP"
+    export CORE_PEER_TLS_ROOTCERT_FILE=$PEER0_PROVIDER_CA
+    export CORE_PEER_MSPCONFIGPATH=${PWD}/organizations/peerOrganizations/provider.bcfm.com/users/Admin@provider.bcfm.com/msp
+    export CORE_PEER_ADDRESS=peer0.provider.bcfm.com:15051
+    export CORE_PEER_TLS_CERT_FILE=${PWD}/organizations/peerOrganizations/provider.bcfm.com/peers/peer0.provider.bcfm.com/tls/server.crt
+    export CORE_PEER_TLS_KEY_FILE=${PWD}/organizations/peerOrganizations/provider.bcfm.com/peers/peer0.provider.bcfm.com/tls/server.key
+  elif [ $USING_ORG -eq 6 ]; then
+    export CORE_PEER_LOCALMSPID="FarmacyMSP"
+    export CORE_PEER_TLS_ROOTCERT_FILE=$PEER0_FARMACY_CA
+    export CORE_PEER_MSPCONFIGPATH=${PWD}/organizations/peerOrganizations/farmacy.bcfm.com/users/Admin@farmacy.bcfm.com/msp
+    export CORE_PEER_ADDRESS=peer0.farmacy.bcfm.com:17051
+    export CORE_PEER_TLS_CERT_FILE=${PWD}/organizations/peerOrganizations/farmacy.bcfm.com/peers/peer0.farmacy.bcfm.com/tls/server.crt
+    export CORE_PEER_TLS_KEY_FILE=${PWD}/organizations/peerOrganizations/farmacy.bcfm.com/peers/peer0.farmacy.bcfm.com/tls/server.key
   else
     errorln "ORG Unknown"
   fi
@@ -69,12 +94,39 @@ parsePeerConnectionParameters() {
   PEERS=""
   while [ "$#" -gt 0 ]; do
     setGlobals $1
-    PEER="peer0.org$1"
+    PEER=""
+    if [ $1 -eq 1 ]; then
+      PEER="peer0.ua"
+    elif [ $1 -eq 2 ]; then
+      PEER="peer0.agency"
+    elif [ $1 -eq 3 ]; then
+      PEER="peer0.transport"
+    elif [ $1 -eq 4 ]; then
+      PEER="peer0.producer"
+    elif [ $1 -eq 5 ]; then
+      PEER="peer0.provider"
+    elif [ $1 -eq 6 ]; then
+      PEER="peer0.farmacy"
+    fi
     ## Set peer addresses
     PEERS="$PEERS $PEER"
     PEER_CONN_PARMS="$PEER_CONN_PARMS --peerAddresses $CORE_PEER_ADDRESS"
     ## Set path to TLS certificate
-    TLSINFO=$(eval echo "--tlsRootCertFiles \$PEER0_ORG$1_CA")
+    TLSINFO=""
+    if [ $1 -eq 1 ]; then
+      TLSINFO=$(eval echo "--tlsRootCertFiles \$PEER0_UA_CA")
+    elif [ $1 -eq 2 ]; then
+      TLSINFO=$(eval echo "--tlsRootCertFiles \$PEER0_AGENCY_CA")
+    elif [ $1 -eq 3 ]; then
+      TLSINFO=$(eval echo "--tlsRootCertFiles \$PEER0_TRANSPORT_CA")
+    elif [ $1 -eq 4 ]; then
+      TLSINFO=$(eval echo "--tlsRootCertFiles \$PEER0_PRODUCER_CA")
+    elif [ $1 -eq 5 ]; then
+      TLSINFO=$(eval echo "--tlsRootCertFiles \$PEER0_PROVIDER_CA")
+    elif [ $1 -eq 6 ]; then
+      TLSINFO=$(eval echo "--tlsRootCertFiles \$PEER0_FARMACY_CA")
+    fi
+    # TLSINFO=$(eval echo "--tlsRootCertFiles \$PEER0_ORG$1_CA")
     PEER_CONN_PARMS="$PEER_CONN_PARMS $TLSINFO"
     # shift by one to get to the next organization
     shift
