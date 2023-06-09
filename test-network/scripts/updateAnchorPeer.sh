16abcc6f26c36c84f9ec1be5db5a3e186ca72951
Ubuntu <ubuntu@ip-172-31-68-21.eu-west-1.compute.internal>
2023-06-09 13:11:37 +0000

    Changes config

--- test-network/scripts/updateAnchorPeer.sh
+++ test-network/scripts/updateAnchorPeer.sh
@@ -2,8 +2,9 @@
 # NOTE: this must be run in a CLI container
 CHANNEL_NAME=$1
 CORE_PEER_LOCALMSPID=$2
+ORDERER_IP=$3
 
-peer channel update -o orderer.bcfm.com:7050 \
+peer channel update -o ${ORDERER_IP}:7050 \
  --ordererTLSHostnameOverride orderer.bcfm.com \
  -c $CHANNEL_NAME -f ./channel-artifacts/${CORE_PEER_LOCALMSPID}anchors.tx --tls \
  --cafile $ORDERER_CA 
