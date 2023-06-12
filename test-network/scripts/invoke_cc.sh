9f412102d7d541afc095e72887bed28ba89aa156
Ubuntu <ubuntu@ip-172-31-68-21.eu-west-1.compute.internal>
2023-06-12 18:36:51 +0000

    New config

--- test-network/scripts/invoke_cc.sh
+++ test-network/scripts/invoke_cc.sh
@@ -4,5 +4,5 @@ echo "Invoke chaincode "
 # peer chaincode invoke -o orderer.bcfm.com:7050 --tls --cafile $ORDERER_CA -C $CHANNEL_NAME -n ${CC_NAME} $PEER_CONN_PARMS --isInit -c ${fcn_call}
 
 
-fcn_call='{"function":"CreateAsset","Args":["1","2","2","2","2"]}'
+fcn_call='{"function":"CreateAsset","Args":["4","2","2","2","2"]}'
 peer chaincode invoke -o orderer.bcfm.com:7050 --tls --cafile $ORDERER_CA -C mychannel -n ${CC_NAME} $PEER_CONN_PARMS -c ${fcn_call}
