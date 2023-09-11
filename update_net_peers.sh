9749d653c67575564f915f6a853e493e0149945a
Ubuntu <ubuntu@ip-172-31-68-21.eu-west-1.compute.internal>
2023-09-11 14:21:43 +0000

    Updated scripts

--- update_net_peers.sh
+++ update_net_peers.sh
@@ -8,6 +8,25 @@ function peerJoinChannel(){
     if [[ $2 = false || $1 = false ]]; then
         echo "Peer name is empty or doesn't match. Indicate the new peer you want to add: './update_net_peers.sh org (org format: Producer, Provider, Agency, Farmacy or Transport)'"
     else
+        echo "Updating services";
+
+        ##OBLIGATORIO USAR UPDATE SERVICE
+        services="$(docker service ls -f Name=bcfm -q)"
+        servicesArray=($services)
+
+        for service in ${servicesArray[@]}
+        do
+            docker service update ${service}
+        done
+        
+        # export SERVICE_RAW=($(cat test-network/docker/docker-compose-test-net.yaml | grep producerbcfm)[])
+        # export SERVICE=$(echo $SERVICE_RAW | tr -d "${SERVICE_RAW:11:-1}")
+
+        # docker service update $SERVICE
+        # docker stack deploy -c test-network/docker/docker-compose-test-net.yaml -c test-network/docker/docker-compose-couch.yaml bcfmOrgs
+        # docker stack deploy -c test-network/docker/docker-compose-cli.yaml bcfmCli
+        echo "Waiting for services to raise..." 
+        sleep 2
         docker exec -it $(docker ps -aqf "name=$1") peer channel join -b ./channel-artifacts/mychannel.block && ./scripts/updateAnchorPeer.sh mychannel ${$1}MSP orderer.bcfm.com
     fi
     
@@ -15,25 +34,29 @@ function peerJoinChannel(){
 
 function addNodeCommands(){
     matches=true
-    if [[ $2 = false && $7 = "Producer" ]]; then
-        echo "Running commands to add a producer"
-        docker node update --label-add name=producer $1
-    elif [[ $3 = false && $7 = "Provider" ]]; then
-        echo "Running commands to add a provider"
-        docker node update --label-add name=provider $1
-    elif [[ $4 = false && $7 = "Agency" ]]; then
-        echo "Running commands to add a agency"
-        docker node update --label-add name=agency $1
-    elif [[ $5 = false && $7 = "Farmacy" ]]; then
-        echo "Running commands to add a farmacy"
-        docker node update --label-add name=farmacy $1
-    elif [[ $6 = false && $7 = "Transport" ]]; then
-        echo "Running commands to add a transport"
-        docker node update --label-add name=transport $1
+    if [ $(docker node ls --format '{{.Status}}' -f id=$1) = "Ready" ]; then
+        if [[ $2 = false  ]] && [[ $7 = "Producer" ]]; then
+            echo "Running commands to add a producer"
+            docker node update --label-add name=producer $1
+        elif [[ $3 = false && $7 = "Provider" ]]; then
+            echo "Running commands to add a provider"
+            docker node update --label-add name=provider $1
+        elif [[ $4 = false && $7 = "Agency" ]]; then
+            echo "Running commands to add a agency"
+            docker node update --label-add name=agency $1
+        elif [[ $5 = false && $7 = "Farmacy" ]]; then
+            echo "Running commands to add a farmacy"
+            docker node update --label-add name=farmacy $1
+        elif [[ $6 = false && $7 = "Transport" ]]; then
+            echo "Running commands to add a transport"
+            docker node update --label-add name=transport $1
+        else
+            matches=false
+        fi
+        peerJoinChannel $7 ${matches}
     else
-        matches=false
+        echo "Node not active"
     fi
-    peerJoinChannel $7 ${matches}
 }
 
 
@@ -76,9 +99,7 @@ function main(){
     fi
 
 
-    echo "Updating services";
-    docker stack deploy -c test-network/docker/docker-compose-test-net.yaml -c test-network/docker/docker-compose-couch.yaml bcfmOrgs
-    docker stack deploy -c test-network/docker/docker-compose-cli.yaml bcfmCli
+    
 
 
 
